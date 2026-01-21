5202f8aa13639ded2e9f7efcfafb41bdd67019f2
bmc00-05 <cbm87961541@gmail.com>
2026-01-21 13:09:36 +0900

    BASE UPLOAD

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1768965764',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
